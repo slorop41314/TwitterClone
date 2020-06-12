157e591db7c1ed2fb2a90875cf3701454a3d1066
Albert Stanley <albertstanleyy@gmail.com>
2020-06-12 20:40:36 +0700

    Initial Commit

---
+++ twitterclone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  twitterclone
+//
+//  Created by Albert Stanley on 12/06/20.
+//  Copyright © 2020 slorop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
